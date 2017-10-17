79fa21672c02527b4e9dabba711fc0dbbd3dec06
Mikhail Orlov <orloffm@gmail.com>
2017-10-17 22:03:12 +0200

    gitref node paket

---
+++ src/GitWriter.Core/Nodes/GitRef.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+
+namespace GitWriter.Core.Nodes
+{
+    public class GitRef : IEquatable<GitRef>
+    {
+        private const string TagPrefix = @"tag: ";
+        private const string OriginPrefix = @"origin/";
+
+        private GitRef()
+        {
+            IsTag = false;
+            IsCommit = true;
+        }
+
+        public string Value { get; private set; }
+        public bool IsTag { get; private set; }
+
+        public bool IsCommit
+        {
+            get => !IsTag;
+            set => IsTag = !value;
+        }
+
+        public bool IsOriginBranch { get; private set; }
+
+        public bool IsLocalBranch => IsCommit && !IsOriginBranch;
+
+        public string ObjectType { get; set; }
+
+        public static GitRef FromForEachRef(string refName,
+            string objectType)
+        {
+            GitRef c = new GitRef();
+            if (refName.StartsWith(OriginPrefix))
+            {
+                refName = refName.Substring(OriginPrefix.Length);
+                c.IsOriginBranch = true;
+            }
+            if (objectType == "tag")
+            {
+                c.IsTag = true;
+            }
+            c.Value = refName;
+            return c;
+        }
+
+        public static GitRef FromGitLogRefName(string refNameEx)
+        {
+            GitRef c = new GitRef();
+            if (refNameEx.StartsWith(TagPrefix))
+            {
+                refNameEx = refNameEx.Substring(TagPrefix.Length);
+                c.IsTag = true;
+            }
+            else if (refNameEx.StartsWith(OriginPrefix))
+            {
+                refNameEx = refNameEx.Substring(OriginPrefix.Length);
+                c.IsOriginBranch = true;
+            }
+            c.Value = refNameEx;
+            return c;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (IsTag)
+            {
+                sb.Append(TagPrefix);
+            }
+            sb.Append(Value);
+            return sb.ToString();
+        }
+
+        #region equality
+
+        public static bool AreEqual(GitRef a,
+            GitRef b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+            return string.Equals(a.Value, b.Value) &&
+                   a.IsCommit == b.IsCommit &&
+                   a.IsOriginBranch == b.IsOriginBranch;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return AreEqual(this, obj as GitRef);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(GitRef a,
+            GitRef b)
+        {
+            return AreEqual(a, b);
+        }
+
+        public static bool operator !=(GitRef a,
+            GitRef b)
+        {
+            return !AreEqual(a, b);
+        }
+
+        public bool Equals(GitRef other)
+        {
+            return AreEqual(this, other);
+        }
+
+        #endregion
+    }
+}
---
+++ src/GitWriter.Core/Nodes/Node.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitWriter.Core.Nodes
+{
+    public class Node
+    {
+        public Node(string hash)
+        {
+            Parents = new List<Node>();
+            Children = new List<Node>();
+            Hash = hash;
+            SetCaptions(null);
+        }
+
+        public List<Node> Parents { get; }
+        public List<Node> Children { get; }
+        public string Hash { get; }
+        public GitRef[] GitRefs { get; private set; }
+        public DateTime Time { get; set; }
+        public int Insertions { get; set; }
+        public int Deletions { get; set; }
+        public bool SomethingWasMergedInto { get; private set; }
+
+        public bool HasTagsOrNonLocalBranches
+        {
+            get { return GitRefs.Any(c => !c.IsLocalBranch); }
+        }
+
+        public void AddChild(Node immediateChild)
+        {
+            if (!Children.Contains(immediateChild))
+            {
+                Children.Add(immediateChild);
+            }
+        }
+
+        public void AddParent(Node immediateParent)
+        {
+            if (!Parents.Contains(immediateParent))
+            {
+                Parents.Add(immediateParent);
+            }
+        }
+
+        public IEnumerable<Node> EnumerateFirstParentsUpTheTreeBranchAgnostic(bool includeSelf = false)
+        {
+            if (includeSelf)
+            {
+                yield return this;
+            }
+            Node n = Parents.FirstOrDefault();
+            while (n != null)
+            {
+                yield return n;
+                n = n.Parents.FirstOrDefault();
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Hash.GetHashCode();
+        }
+
+        public void RemoveItselfFromTheNodeGraph()
+        {
+            // Parents.
+            foreach (Node parent in Parents)
+            {
+                // Remove node.
+                parent.Children.Remove(this);
+                // Set its children to this parent.
+                foreach (Node immediateChild in Children)
+                {
+                    parent.AddChild(immediateChild);
+                }
+            }
+            // Children.
+            foreach (Node child in Children)
+            {
+                // Remove the node from parents, child.Parents.Remove(this);
+                // Set its parents to this child.
+                foreach (Node immediateParent in Parents)
+                {
+                    child.AddParent(immediateParent);
+                }
+            }
+            Node firstChild = Children.FirstOrDefault();
+            if (firstChild != null)
+            {
+                firstChild.Insertions += Insertions;
+                firstChild.Deletions += Deletions;
+                firstChild.SetAsSomethingWasMergedInto();
+            }
+            // Clear references.
+            Parents.Clear();
+            Children.Clear();
+        }
+
+        public void SetAsSomethingWasMergedInto()
+        {
+            SomethingWasMergedInto = true;
+        }
+
+        public void SetCaptions(string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                GitRefs = new GitRef[0];
+                return;
+            }
+
+            source = source.Trim();
+            source = source.TrimStart('(');
+            source = source.TrimEnd(')');
+
+            string[] captionStrings = source.Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+            GitRefs = captionStrings.Select(GitRef.FromGitLogRefName).ToArray();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Time.ToString("D"));
+            sb.Append(": ");
+            if (Parents.Count > 0)
+            {
+                for (int index = 0; index < Parents.Count; index++)
+                {
+                    Node p = Parents[index];
+                    if (index > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append(p.Hash);
+                }
+                sb.Append(" -> ");
+            }
+            sb.Append("[" + Hash + "]");
+            if (Children.Count > 0)
+            {
+                sb.Append(" -> ");
+                for (int index = 0; index < Children.Count; index++)
+                {
+                    Node c = Children[index];
+                    if (index > 0)
+                    {
+                        sb.Append(", ");
+                    }
+                    sb.Append(c.Hash);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
