f8c5743d87ad6ae5eb04ca6d4583ecef46704aed
Mikhail Orlov <orloffm@gmail.com>
2018-11-12 20:15:25 +0100

    logging picker

--- src/Prigitsk.Core/Graph/BranchPicker.cs
+++ src/Prigitsk.Core/Graph/BranchPicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 
 namespace Prigitsk.Core.Graph
 {
@@ -8,27 +9,67 @@ namespace Prigitsk.Core.Graph
     {
         private readonly Regex[] _excludeRegices;
         private readonly Regex[] _includeRegices;
+        private readonly ILogger<BranchPicker> _logger;
 
-        public BranchPicker(IBranchPickingOptions options)
+        public BranchPicker(IBranchPickingOptions options, ILogger<BranchPicker> logger)
         {
+            _logger = logger;
+
+            _logger.Debug("Loading exclude branches regular expressions.");
             _excludeRegices = CreateRegices(options.ExcludeBranchesRegices);
+            _logger.Debug("Finished loading exclude branches regular expressions.");
+
+            _logger.Debug("Loading include branches regular expressions.");
             _includeRegices = CreateRegices(options.IncludeBranchesRegices);
+            _logger.Debug("Finished loading include branches regular expressions.");
         }
 
         public bool ShouldBePicked(string branchLabel)
         {
+            Regex matchedBy;
+
             if (_includeRegices != null)
             {
-                bool isExplicitInclude = _includeRegices.Any(r => r.IsMatch(branchLabel));
-                return isExplicitInclude;
+                matchedBy = _includeRegices.FirstOrDefault(r => r.IsMatch(branchLabel));
+                if (matchedBy != null)
+                {
+                    _logger.Debug(
+                        "{0} - picked (matched by explicit include regex {1}).",
+                        branchLabel,
+                        matchedBy.ToString()
+                    );
+                    return true;
+                }
+
+                _logger.Debug(
+                    "{0} - not picked (not matched by any of the explicit include regices that are specified).",
+                    branchLabel,
+                    matchedBy.ToString()
+                );
+                return false;
             }
 
             if (_excludeRegices != null)
             {
-                bool isExplicitExclude = _excludeRegices.Any(r => r.IsMatch(branchLabel));
-                return !isExplicitExclude;
+                matchedBy = _excludeRegices.FirstOrDefault(r => r.IsMatch(branchLabel));
+                if (matchedBy != null)
+                {
+                    _logger.Debug(
+                        "{0} - not picked (matched by explicit exclude regex {1}; there are no include regices).",
+                        branchLabel,
+                        matchedBy.ToString()
+                    );
+                    return false;
+                }
+
+                _logger.Debug(
+                    "{0} - picked (not matched by any of the explicit exclude regices that are specified; there are no include regices).",
+                    branchLabel
+                );
+                return true;
             }
 
+            _logger.Debug("{0} - picked (no explicit exclude or include regices specified).", branchLabel);
             return true;
         }
 
@@ -47,6 +88,7 @@ namespace Prigitsk.Core.Graph
                     continue;
                 }
 
+                _logger.Debug(" {0}", regexString);
                 Regex r = new Regex(regexString, RegexOptions.IgnoreCase | RegexOptions.Compiled);
                 regices.Add(r);
             }
