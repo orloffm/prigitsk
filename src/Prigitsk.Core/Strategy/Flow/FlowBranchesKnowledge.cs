aa1bb31dca1683a2316d8f774db361c11e11d249
Mikhail Orlov <orloffm@gmail.com>
2018-11-12 21:04:02 +0100

    proper order

--- src/Prigitsk.Core/Strategy/Flow/FlowBranchesKnowledge.cs
+++ src/Prigitsk.Core/Strategy/Flow/FlowBranchesKnowledge.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using Microsoft.Extensions.Logging;
 using Prigitsk.Framework;
 
 namespace Prigitsk.Core.Strategy.Flow
@@ -10,42 +11,56 @@ namespace Prigitsk.Core.Strategy.Flow
         private readonly IMultipleDictionary<BranchLogicalType, string> _typesToRegices;
 
         public FlowBranchesKnowledge(
-            ILesserBranchRegex workItemRegex,
-            IWorkItemBranchSelectorFactory workItemBranchSelectorFactory
-        ) : base(workItemRegex, workItemBranchSelectorFactory)
+            ILesserBranchRegex workItemRegex
+            , IWorkItemBranchSelectorFactory workItemBranchSelectorFactory
+            , ILogger<FlowBranchesKnowledge> logger
+        ) : base(workItemRegex, workItemBranchSelectorFactory, logger)
         {
             _typesToColors = new Dictionary<BranchLogicalType, Color>
             {
-                // Master goes first, because we need to see "these guys are on PROD".
-                {BranchLogicalType.Master, Color.FromArgb(82, 0, 198)},
-                // Hotfix are on top of master.
-                {BranchLogicalType.MasterHotfix, Color.FromArgb(253, 89, 101)},
-                // What will get in the next release?
-                {BranchLogicalType.Release, Color.FromArgb(82, 195, 34)},
-                // What is integrated and planned for the next release?
-                {BranchLogicalType.Integration, Color.FromArgb(127, 171, 232)},
-                // What is in develop that is not in any other branches?
-                {BranchLogicalType.Develop, Color.FromArgb(234, 175, 0)},
-                // What are the features developed on top of develop?
-                {BranchLogicalType.DevelopFeature, Color.FromArgb(186, 83, 27)},
-                // What are the work items developed on top of develop.
-                {BranchLogicalType.WorkItem, Color.FromArgb(201, 130, 175)}
+                {BranchLogicalType.Master, Color.FromArgb(82, 0, 198)}
+                , {BranchLogicalType.MasterHotfix, Color.FromArgb(253, 89, 101)}
+                , {BranchLogicalType.Release, Color.FromArgb(82, 195, 34)}
+                , {BranchLogicalType.Integration, Color.FromArgb(127, 171, 232)}
+                , {BranchLogicalType.Develop, Color.FromArgb(234, 175, 0)}
+                , {BranchLogicalType.DevelopFeature, Color.FromArgb(186, 83, 27)}
+                , {BranchLogicalType.WorkItem, Color.FromArgb(201, 130, 175)}
             };
 
             _typesToRegices = new MultipleDictionary<BranchLogicalType, string>
             {
-                {BranchLogicalType.Master, new[] {"^master$"}},
-                {BranchLogicalType.Develop, new[] {"^develop$"}},
-                {BranchLogicalType.DevelopFeature, new[] {"^dev", "-dev-"}},
-                {BranchLogicalType.Release, new[] {"^release", "-RC$"}},
-                {BranchLogicalType.MasterHotfix, new[] {"hotfix", "^master"}},
-                {BranchLogicalType.Integration, new[] {"^int"}}
+                {BranchLogicalType.Master, new[] {"^master$"}}, {BranchLogicalType.Develop, new[] {"^develop$"}}
+                , {BranchLogicalType.DevelopFeature, new[] {"^dev", "-dev-"}}
+                , {BranchLogicalType.Release, new[] {"^release", "-RC$"}}
+                , {BranchLogicalType.MasterHotfix, new[] {"hotfix", "^master"}}
+                , {BranchLogicalType.Integration, new[] {"^int"}}
             };
         }
 
         protected override Color GetColorInternal(BranchLogicalType ft)
         {
             return _typesToColors[ft];
+        }
+
+        protected override BranchLogicalType[] GetAllBranchLogicalTypesOrdered()
+        {
+            return new[]
+            {
+                // Master goes first, because we need to see "this is on PROD".
+                BranchLogicalType.Master
+                // Hotfix are on top of master.
+                , BranchLogicalType.MasterHotfix
+                // What will get in the next release?
+                , BranchLogicalType.Release
+                // What is integrated and planned for the next release?
+                , BranchLogicalType.Integration
+                // What is in develop that is not in any other branches?
+                , BranchLogicalType.Develop
+                // What are the features developed on top of develop?
+                , BranchLogicalType.DevelopFeature
+                // What are the work items developed on top of develop.
+                , BranchLogicalType.WorkItem
+            };
         }
 
         protected override bool TryGetRegexStringsInternal(BranchLogicalType flowType, out ISet<string> regexStrings)
